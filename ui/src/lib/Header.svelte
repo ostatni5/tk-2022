ed8a9338c2bd2a72578b44dd40f840d53e2c67f1
michal367 <michal.krawczyk7@gmail.com>
2022-03-23 11:46:18 +0100

    add basic form for image parameters

---
+++ ui/src/lib/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<header>
+	<h1>ImageFinder</h1>
+</header>
+
+<style>
+	header h1 {
+		text-align: center;
+		color: #222;
+		font-style: italic;
+	}
+</style>
--- ui/src/routes/__layout.svelte
+++ ui/src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
+	import Header from '$lib/Header.svelte';
 	import '../app.css';
 </script>
 
@@ -10,7 +10,6 @@
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 </footer>
 
 <style>
@@ -31,10 +30,6 @@
 		justify-content: center;
 		align-items: center;
 		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
