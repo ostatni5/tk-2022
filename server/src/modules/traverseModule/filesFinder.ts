819d77b24413034c1e0ad3244d7243f10c59f5b0
michal367 <michal.krawczyk7@gmail.com>
2022-03-28 13:12:02 +0200

    add files and images generators

--- server/src/modules/traverseModule/filesFinder.ts
+++ server/src/modules/traverseModule/filesFinder.ts
@@ -3,24 +3,42 @@ import path from 'path';
 import imageType from 'image-type';
 
 export const getAllFiles = (baseFolder: string, recursive: boolean = true, filesList: string[] = []): string[] => {
-	let currDirFiles: string[] = fs.readdirSync(baseFolder);
-	let absoluteFiles: string[] = currDirFiles.map((file: string) => path.join(baseFolder, file))
+    const currDirFiles: string[] = fs.readdirSync(baseFolder);
+    const absoluteFiles: string[] = currDirFiles.map((file: string) => path.join(baseFolder, file))
 
-	let regularFiles: string[] = absoluteFiles.filter((file: string) => fs.statSync(file).isFile());
-	filesList.push(...regularFiles);
+    const regularFiles: string[] = absoluteFiles.filter((file: string) => fs.statSync(file).isFile());
+    filesList.push(...regularFiles);
 
-	if (recursive){
-		absoluteFiles.forEach((file: any) => {
-			if (fs.statSync(file).isDirectory()){
-				getAllFiles(file, recursive, filesList);
-			}
-		});
-	}
-	return filesList;
+    if (recursive){
+        absoluteFiles.forEach((file: string) => {
+            if (fs.statSync(file).isDirectory()){
+                getAllFiles(file, recursive, filesList);
+            }
+        });
+    }
+    return filesList;
+}
+
+export function* directoryFilesGenerator(baseFolder: string, recursive: boolean = true): IterableIterator<string> {
+    const currDirFiles: string[] = fs.readdirSync(baseFolder);
+    const absoluteFiles: string[] = currDirFiles.map((file: string) => path.join(baseFolder, file))
+
+    const regularFiles: string[] = absoluteFiles.filter((file: string) => fs.statSync(file).isFile());
+    for (const file of regularFiles){
+        yield file;
+    }
+
+    if (recursive){
+        for (const file of absoluteFiles){
+            if (fs.statSync(file).isDirectory()){
+                yield* directoryFilesGenerator(file, recursive);
+            }
+        }
+    }
 }
 
 const readChunk = (file: string, size: number): Buffer => {
-    let buffer = Buffer.alloc(size);
+    const buffer = Buffer.alloc(size);
     const fd: number = fs.openSync(file, 'r')
     try{
         const bytesRead: number = fs.readSync(fd, buffer, 0, size, null);
@@ -39,4 +57,12 @@ export const isImage = (file: string) => {
 export const getAllImages = (baseFolder: string, recursive: boolean = true) => {
     const files = getAllFiles(baseFolder, recursive);
     return files.filter((file: string) => isImage(file));
+}
+
+export function* directoryImagesGenerator(baseFolder: string, recursive: boolean = true): IterableIterator<string> {
+    for(const file of directoryFilesGenerator(baseFolder, recursive)){
+        if (isImage(file)){
+            yield file;
+        }
+    }
 }
