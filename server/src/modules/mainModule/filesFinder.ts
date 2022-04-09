f8fb8d48cc3f883d759fead702a05832e16983b1
michal367 <michal.krawczyk7@gmail.com>
2022-04-09 17:36:26 +0200

    fix filesFinder permission error (#49)

--- server/src/modules/mainModule/filesFinder.ts
+++ server/src/modules/mainModule/filesFinder.ts
@@ -3,26 +3,47 @@ import path from 'path';
 import imageType from 'image-type';
 
 export function* directoryFilesGenerator(baseFolder: string, recursive: boolean = true): IterableIterator<string> {
-    const currDirFiles: string[] = fs.readdirSync(baseFolder);
-    const absoluteFiles: string[] = currDirFiles.map((file: string) => path.join(baseFolder, file))
+    let currDirFiles: string[] = [];
+    try{
+        currDirFiles = fs.readdirSync(baseFolder);
+    }
+    catch{
+        return;
+    }
+    const absolutePaths: string[] = currDirFiles.map((file: string) => path.join(baseFolder, file));
 
-    const regularFiles: string[] = absoluteFiles.filter((file: string) => fs.statSync(file).isFile());
-    for (const file of regularFiles){
-        yield file.split('\\').join('/');
+    const directoryPaths: string[] = [];
+    for(const file of absolutePaths){
+        try{
+            const stat: fs.Stats = fs.statSync(file);
+            if(stat?.isFile()){
+                yield file.split('\\').join('/');
+            }
+            else if(stat?.isDirectory()){
+                directoryPaths.push(file);
+            }
+        }
+        catch{
+            continue;
+        }
     }
 
     if (recursive){
-        for (const file of absoluteFiles){
-            if (fs.statSync(file).isDirectory()){
-                yield* directoryFilesGenerator(file, recursive);
-            }
+        for (const dir of directoryPaths){
+            yield* directoryFilesGenerator(dir, recursive);
         }
     }
 }
 
 const readChunk = (file: string, size: number): Buffer => {
     const buffer = Buffer.alloc(size);
-    const fd: number = fs.openSync(file, 'r')
+    let fd: number = 0;
+    try{
+        fd = fs.openSync(file, 'r');
+    }
+    catch{
+        return buffer;
+    }
     try{
         const bytesRead: number = fs.readSync(fd, buffer, 0, size, null);
         return bytesRead < size ? buffer.slice(0, bytesRead) : buffer;
