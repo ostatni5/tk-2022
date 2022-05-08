ec5478e1134e49a0092fd796867453c704033f2a
retkiewi <krzret@student.agh.edu.pl>
2022-05-08 14:11:20 +0200

    update start scripts

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
 invoke-expression 'cmd /c start powershell -Command { npm run start -w=ui }'
-invoke-expression 'cmd /c start powershell -Command { npm run main -w=server }'
-invoke-expression 'cmd /c start powershell -Command { npm run metadata -w=server }'
+invoke-expression 'cmd /c start powershell -Command { npm run main -w=node_servers }'
+invoke-expression 'cmd /c start powershell -Command { npm run metadata -w=node_servers }'
 invoke-expression 'cmd /c start powershell -Command { cd text_server; mix run --no-halt }'
