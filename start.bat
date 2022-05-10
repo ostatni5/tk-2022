62fb9900c3a782713a37dbcce9f4a079fb47692e
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2022-05-10 11:26:47 +0200

    update start.bat

--- start.bat
+++ start.bat
@@ -3,5 +3,5 @@ start "server-main" cmd.exe /k "npm run main -w=node_servers"
 start "server-metadata" cmd.exe /k "npm run metadata -w=node_servers"
 start "server-text" cmd.exe /k "cd text_server && mix run --no-halt"
 start "server-people" cmd.exe /k "cd .\people_server\build\bin\ && .\peopleServer.exe"
-start "server-weather" cmd.exe /k "weather_server/env/Scripts/Activate.bat && python weather_server"
+start "server-weather" cmd.exe /k "cd weather_server\env\Scripts && activate && cd ..\..\.. && python weather_server"
 
